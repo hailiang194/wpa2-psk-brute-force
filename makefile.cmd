89f134040d60c460ab8213a02bb6199ef1f7b40d
= <sonlaneedsme@outlook.com>
2019-06-15 23:47:17 +0700

    Compile wifi-list-type.c and list-wifi.c

--- makefile.cmd
+++ makefile.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 :COMPILE
-echo "makefile " %1
-gcc %1 preprocess.c write-xml.c brute-force-wifi.c -o ex.exe
+echo makefile  %1
+gcc %1 preprocess.c write-xml.c brute-force-wifi.c list-wifi-type.c list-wifi.c -o ex.exe
 EXIT /B 0
--- test.c
+++ test.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "preprocess.h"
-
 
 /*
  * THIS FILE IS USED TO TEST
