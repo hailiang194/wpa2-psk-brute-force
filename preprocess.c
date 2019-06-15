64fa8a277455c67b26ba87a86c0bac2fcc205dcd
= <sonlaneedsme@outlook.com>
2019-06-15 21:03:09 +0700

    Add two function strip() and firstNumInStr()

--- preprocess.c
+++ preprocess.c
@@ -17,3 +17,62 @@ void strToHex(char *str, char *hexStr)
 	//insert NULL at the end of the output string
 	hexStr[i++] = '\0';
 }
+
+int firstNumInStr(char *str)
+{
+	int num = 0;
+	char *ref = str;
+	const int len = strlen(str);
+	
+	//find first digit
+
+	short findDigit = 0; //check if find first digit
+
+	while(ref != str + (len - 1))
+	{
+		if(isdigit(*ref))
+		{
+			findDigit = 1;
+			num *= 10;
+			num += ((*ref) - '0');
+		}
+
+		if(!isdigit(*ref) && findDigit)
+			break;
+
+		ref++;
+	}
+
+	return num;
+}
+
+void strip(char *str)
+{
+	char *ref = str;
+
+	//remove at the leading
+	while(*ref != '\0')
+	{
+		if(!isspace(*ref))
+		{
+			strcpy(str, ref);
+			break;
+		}
+		ref++;
+	}
+
+	//remove at the trailing
+	ref = str + (strlen(str) - 1);
+
+	while(ref >= str)
+	{
+		if(!isspace(*ref))
+		{
+			*(ref + 1) = '\0';
+			break;
+		}
+
+		ref--;
+	}
+
+}
--- preprocess.h
+++ preprocess.h
@@ -1,10 +1,18 @@
 #ifndef PREPROCESS_H
 	#define PREPROCESS_H
 #include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 
 //Convert ascii string to hex-string
 //Note: (length of hexStr) >= 2 * (length of str) + 1
 void strToHex(char *str, char *hexStr);
+
+//return the first number appear in str
+//if there is no number in st, this function will return 0
+int firstNumInStr(char *str);
+
+//remove all the leading and trailing white-spaces from a string
+void strip(char *str);
 
 #endif
